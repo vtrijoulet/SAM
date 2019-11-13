35b39434463cbfeccbd66c6e5081e21ba3c43a02
Vanessa Trijoulet <vtri@win.dtu.dk>
2019-11-13 14:59:50 +0100

    Function to fit hockey-stick from a SAM fit

---
+++ stockassessment/R/HS_fit.R
@@ -0,0 +1,71 @@
+
+HS_fit <- function(name, rec.years=NULL){
+
+  fit=fitfromweb(name,character.only=TRUE)
+  if (missing(rec.years)) rec.years=1:fit$data$noYears else rec.years=which(fit$data$years%in%rec.years) # choose SR pairs
+
+  y=rectable(fit)[rec.years[-1],1]
+  x=ssbtable(fit)[rec.years[-length(rec.years)],1]
+
+  fn <- function(par){
+    i <- (x < exp(par[1]))
+    slope <- exp(par[2]) / exp(par[1])
+    sum( (log(y[i]) - log(slope * x[i]))^ 2 ) + sum( ( log(y[!i]) - par[2] )^ 2  )
+  }
+  n = 100
+  gr <- expand.grid(x=seq(log(min(x)),log(max(x)),length=n), y=seq(log(min(y)),log(max(y)),length=n))
+  m <- apply(gr,1,fn)
+  # Get close to global optimum
+  par = unlist(gr[which.min(m),]) # par are x,y coordinates for inflexion point
+  # Find the exact global optimum
+  opt <- optim(par, fn)
+
+
+  log.pred.y <- y
+  i <- (x < exp(opt$par[1]))
+  slope <- exp(opt$par[2]) / exp(opt$par[1])
+  log.pred.y[i] <- log(slope * x[i])
+  log.pred.y[!i] <-(opt$par[2])
+
+  sd.log.y <- sd(log(y)-log.pred.y)
+  # CI.low <- log.pred.y-1.96*sd.log.y
+  # CI.up <- log.pred.y+1.96*sd.log.y
+  # 
+  # plot(y=log(y), x=log(x), pch=16, type="p")
+  # lines(log.pred.y~log(x))
+  # polygon(x=c(sort(log(x)),rev(sort(log(x)))), y=c(sort(CI.low),rev(sort(CI.up))), col=rgb(0,0,0,alpha=0.2), border = NA)
+  # 
+  # plot(y=y, x=x, pch=16, type="o", col="red")
+  # lines(exp(log.pred.y)~x)
+  # polygon(x=c(sort(x),rev(sort(x))), y=c(sort(exp(CI.low)),rev(sort(exp(CI.up)))), col=rgb(0,0,0,alpha=0.2), border = NA)
+
+  return(c(as.vector(opt$par), sd.log.y))
+  #save.image(file=paste0("~/DTU/Manuscripts/MSE_forecast_paper/results/HS_param_", name, ".Rdata"))
+  
+}
+
+
+
+# fn1 <- function(par, ssb,rec){
+#   log_pred_rec <- vector(length=length(ssb))
+#   for (t in 1:length(ssb)){
+#       if (ssb[t]>=(exp(par[1])/exp(par[2]))){
+#         log_pred_rec[t] <- log(exp(par[1]))
+#       } else {
+#         log_pred_rec[t] <- log(exp(par[2])*ssb[t])
+#       }
+#   }
+#   nll=sum((log(rec)-log_pred_rec)^2)
+#   #nll=nll+plogis(log(exp(par[1]))-log(max(rec)))*1e10*(log(exp(par[1]))-log(max(rec)))^2
+#   #if (log(exp(par[1]))>log(max(rec))) nll=nll+1e10*(log(exp(par[1]))-log(max(rec)))^2
+#   #nll=nll+plogis(min(ssb)-(exp(par[1])/exp(par[2])))*1e10*(min(ssb)-(exp(par[1])/exp(par[2])))^2
+#   #if ((exp(par[1])/exp(par[2]))<=min(ssb)) nll=nll+1e10*(min(ssb)-(exp(par[1])/exp(par[2])))^2
+#   return(nll)
+# }
+# opt <- optim(par = c(log(max(rectable(fit)[rec.years[-1],1])), log(10))*1, ssb=ssbtable(fit)[rec.years[-length(rec.years)],1], rec=rectable(fit)[rec.years[-1],1], fn=fn1, control =list(reltol=1e-40, maxit=1000) )
+# exp(opt$par)
+# plot(y=rec, x=ssb, pch=16, type="o", xlim=c(0,max(ssbtable(fit)[-fit$data$noYears,1])))
+# pred.rec=exp(opt$par[2])*seq(0,300000,100)
+# lines(pred.rec~seq(0,300000,100))
+# abline(h=exp(opt$par[1]))
+
